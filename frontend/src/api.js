8ac31a55b101d4e0c7a0f26623631c67a536edd1
pratimaVN <pratimanagannavar@gmail.com>
2025-12-15 23:05:59 +0530

    updated frontend with backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: import.meta.env.VITE_API_URL});
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
